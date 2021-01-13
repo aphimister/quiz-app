60ab16963e12576edb1a5bda6051e142e5d084de
Alex Phimister <wphimmy@gmail.com>
2021-01-13 16:23:18 +0000

    Calls to the API and displays one question

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-import Login from './components/Login'
+import Login from './components/Login';
 import Logout from './components/Logout';
-import Home from './components/Home'
+import Home from './components/Home';
+import QuizPage from './components/QuizPage';
 import './App.css';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Nav/>
-    <Switch>
-        <Route exact path="/" component={Home}/>  
-        <Route exact path="/Login" component={Login} />
-        <Route exact path="/Logout" component={Logout} />
-    </Switch>
-  </BrowserRouter>   
-</>
-  )
-
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/quiz" component={QuizPage} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/QuizPage.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const QuizPage = () => {
+  const [quiz, setQuiz] = useState(['']);
+
+  const apiURL =
+    'https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple';
+
+  const apiCall = async (url) => {
+    const call = await axios.get(url);
+    setQuiz(call.data.results);
+  };
+
+  useEffect(() => {
+    //calls the API on page load
+    apiCall(apiURL);
+  }, []);
+
+  console.log(quiz);
+  console.log(quiz[0].question);
+
+  return (
+    // quiz display
+    <div>
+      <QuizCard
+        question={quiz[0].question}
+        correct_answer={quiz[0].correct_answer}
+        incorrect_answers={quiz[0].incorrect_answers}
+        number={1}
+      />
+    </div>
+  );
+};
+
+const QuizCard = (props) => {
+  const question = props.question;
+  const correctAns = props.correct_answer;
+  const incorrectAns = props.incorrect_answers;
+  const number = props.number;
+
+  if (props.question) {
+    return (
+      <div className="quizCard">
+        <div className="questionHeading">{question}</div>
+        <input
+          type="radio"
+          className="radioButton"
+          name={number}
+          id={incorrectAns}
+        />
+        <label htmlFor={correctAns}>{correctAns}</label>
+        <input
+          type="radio"
+          className="radioButton"
+          name={number}
+          id={incorrectAns[0]}
+        />
+        <label htmlFor={incorrectAns[0]}>{incorrectAns[0]}</label>
+        <input
+          type="radio"
+          className="radioButton"
+          name={number}
+          id={incorrectAns[1]}
+        />
+        <label htmlFor={incorrectAns[1]}>{incorrectAns[1]}</label>
+        <input
+          type="radio"
+          className="radioButton"
+          name={number}
+          id={incorrectAns[2]}
+        />
+        <label htmlFor={incorrectAns[2]}>{incorrectAns[2]}</label>
+      </div>
+    );
+  } else {
+    return <div>Loading questions</div>;
+  }
+};
+
+export default QuizPage;
