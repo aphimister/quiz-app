a719aa3411186d670edf7eb36f7bf818182ef36f
87werner <twerner15@googlemail.com>
2021-01-14 21:22:57 +0000

    register-user-in-mongoDB

--- models/quizUser.js
+++ models/quizUser.js
@@ -13,7 +13,7 @@ const user = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    },
+    }
 
 });
 
--- server.js
+++ server.js
@@ -2,24 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const bcrypt = require("bcryptjs");
 const path = require('path');
-
+const cookieParser = require("cookie-parser");
 const app = express();
+const quiztoken = require('jsonwebtoken')
 const Quizuser = require('./models/quizUser')
+const check = require('./middlewares/check')
 dotenv.config({ path: './.env' });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ extended: false }));
 app.use(cors());
 
 
-mongoose
-  .connect(process.env.DB_URL, {
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log('MongoDB is connected'));
+  }).then(() => console.log('MongoDB is connected'));
 
 
 app.get('/', (req, res) => {
@@ -30,14 +31,19 @@ app.get('/', (req, res) => {
 //<----------------------- USER SECTION --------------------------->
 
 //<----------------------- USER REGISTER --------------------------->
-app.get('/register', async (req, res) => {
+app.post('/register', async (req, res) => {
 
+console.log("at the back end ")
+console.log(req.body)
+//below data grab from front, push through here at on to the DB schema
   await Quizuser.create({
-    name: "john",
-    email: "john@codenation.com",
-    password: 1234
+    name: req.body.userName,
+    email: req.body.userEmail,
+    password: req.body.userPassword
   })
-  res.send("user registered")
+  res.json({
+    message: "user was registered"
+})
 });
 
 //ADMIN BASED SERVER
@@ -48,14 +54,20 @@ app.get('/register', async (req, res) => {
 //<--------------------------Login ----------------------------------------------->>
 
 app.get("/login", (req, res) => {
+  console.log("at the back end ")// lets us know its working
+  console.log(req.body) // front end input field to loggin
   res.json({
     message: "logged in "
-})
+}) // message display at fron end 
 });
 
+app.post("/login", (req,res) => {
+
+})
 
 
 
-app.listen(5000, () => {
+
+app.listen(5001, () => {
   console.log('Server is online');
 });
