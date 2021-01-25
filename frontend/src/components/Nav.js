25df13f45a899528f1522edc59453e3b4bf8cf07
87werner <twerner15@googlemail.com>
2021-01-25 17:07:06 +0000

    nav hover and media

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,34 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const logOutHandler = async () => {
-  await axios.get('/logout');
+  await axios.get("/logout");
 };
 const Nav = (props) => {
   return (
     <nav className="nav">
       <ul className="un-list">
-        <li className="li-list">
-          <Link className="link" to="/">
-            Home
-          </Link>
-        </li>
-        <li className="li-list">
-          <Link className="link" to="/profile" >
-            Profile
-          </Link>
-        </li>
-        <li className="li-list">
-          <Link className="link" to="/login">
-            Login
-          </Link>
-        </li>
-        <li className="li-list">
-          <Link to="/logout" className="link" onClick={logOutHandler}>
-            Logout
-          </Link>
-        </li>
+        <div className="li-container">
+          <div className="li-item-container">
+            <li className="li-list">
+              <Link className="link" to="/">
+                Home
+              </Link>
+            </li>
+          </div>
+          <div className="li-item-container">
+            <li className="li-list">
+              <Link className="link" to="/profile">
+                Profile
+              </Link>
+            </li>
+          </div>
+          <div className="li-item-container">
+            <li className="li-list">
+              <Link className="link" to="/login">
+                Login
+              </Link>
+            </li>
+          </div>
+          <div className="li-item-container">
+            <li className="li-list">
+              <Link to="/logout" className="link" onClick={logOutHandler}>
+                Logout
+              </Link>
+            </li>
+          </div>
+        </div>
         {/* <li className="li-list">
                         <Link className='link-register' to="/register">Register</Link>
                     </li> */}
